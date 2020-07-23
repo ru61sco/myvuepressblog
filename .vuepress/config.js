1a233c0b19602dedb8cdd2c88f1c51b129875166
Liang Jiang <liangj@vt.edu>
2020-07-23 19:14:57 +0800

    added update date
    added update date function on config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,10 +1,22 @@
 module.exports = {
     themeConfig: {
+      lastUpdated: '更新时间',
       logo: '/assets/img/logo.png',
       nav: [
           {text: "Home", link: '/'},
           {text: "Guide", link: '/about'},
-          {text: "External", link: 'https://bing.com'}
-      ]
+          {text: "Bing搜索", link: 'https://bing.com'}
+      ],
+      sidebar: {
+        '/life/':[
+          'travel',
+          'photography',
+          'music',
+        ],
+        '/work/':[
+          'biology',
+          'pharma',
+        ],
+      }
     }
   }
--- about.md
+++ about.md
@@ -1,5 +1,5 @@
 ---
-navbar: false
+navbar: true
 ---
 
 ## 关于我
